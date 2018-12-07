3641c0788f191fd9700ae0dfa7096974b4ed6bc3
Kassandra Falia flowers <kfalia-f@e3r2p12.21-school.ru>
2018-12-07 18:44:04 +0300

    42

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,11 @@
 /*   By: kfalia-f <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/06 15:11:01 by kfalia-f          #+#    #+#             */
-/*   Updated: 2018/12/07 16:58:42 by kfalia-f         ###   ########.fr       */
+/*   Updated: 2018/12/07 18:43:54 by kfalia-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
 char	ft_zap(char *buf, char **line, char **list)
 {
@@ -27,7 +26,7 @@ char	ft_zap(char *buf, char **line, char **list)
 	if (!line || !list)
 		return (-1);
 	return (0);
-}	
+}
 
 int		get_next_line(const int fd, char **line)
 {
@@ -41,9 +40,8 @@ int		get_next_line(const int fd, char **line)
 		list = ft_strnew(0);
 	*line = ft_strnew(0);
 	if (ft_strlen(list) > 0)
-		if (ft_strchr(list, '\n') != NULL)
-			if (ft_zap(list, line, &list) == -1)
-				return (-1);
+		if (ft_strchr(list, '\n') != NULL && ft_zap(list, line, &list) == -1)
+			return (-1);
 	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
 	{
 		if (ft_strchr(buf, '\n') != NULL)
@@ -58,17 +56,4 @@ int		get_next_line(const int fd, char **line)
 	if (ret == 0 && !ft_strchr(list, '\n'))
 		return (0);
 	return (1);
-}
-
-int main()
-{
-	int fd;
-	char *line;
-
-	fd = open("test", O_RDONLY);
-	printf("%d\n%s\n\n", get_next_line(fd, &line), line);
-	printf("%d\n%s\n\n", get_next_line(fd, &line), line);
-	printf("%d\n%s\n\n", get_next_line(fd, &line), line);
-	printf("%d\n%s\n\n", get_next_line(fd, &line), line);
-	return (0);
 }
